d5b6e7b6372d4212e0149c484bfd3b5ade8a053e
Nico <nicolas.linard@gmail.com>
2024-12-03 13:48:22 +0100

    starting day 1 challenge

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,14 +1,22 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
+    let mut list1 = Vec::new();
+    let mut list2 = Vec::new();
+
     for line in input.lines() {
-        // Process each line here
-        println!("{}", line); // Example: print each line
-    }
+        let mut parts = line.split_whitespace();
+        if let (Some(part1), Some(part2)) = (parts.next(), parts.next()) {
+            if let (Ok(num1), Ok(num2)) = (part1.parse::<u32>(), part2.parse::<u32>()) {
+                list1.push(num1);
+                list2.push(num2);
+            }
+        }
+    }    
     None
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
@@ -18,8 +26,8 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        part_one(&advent_of_code::template::read_file("examples", DAY));
+        //assert_eq!(result, None);
     }
 
     #[test]
